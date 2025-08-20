0248d9162c67c28ce4271d59e572ed757da6ec28
JadhavAnsh <jadhavansh10@gmail.com>
2025-08-20 16:52:46 +0530

    Updated dependencies and configure modules in the applications named recipes and notes with normal crud endpoints. Added @nestjs/config, @nestjs/graphql, @nestjs/mapped-types, @nestjs/mongoose, apollo-server-express, firebase-admin, graphql, and mongoose to package.json. Integrated RecipesModule and NotesModule into the AppModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RecipesModule } from './recipes/recipes.module';
+import { NotesModule } from './notes/notes.module';
 
 @Module({
-  imports: [],
+  imports: [RecipesModule, NotesModule],
   controllers: [AppController],
   providers: [AppService],
 })
